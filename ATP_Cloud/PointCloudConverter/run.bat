db733ac914477c9a2f19fe7c35e730ee9468bc32
unknown <amoujhang@13techart.com>
2019-04-17 22:21:45 +0800

    beta

--- ATP_Cloud/PointCloudConverter/run.bat
+++ ATP_Cloud/PointCloudConverter/run.bat
@@ -1,6 +1,4 @@
 SET INPUT=%~1
-SET OUTPUT=%~nx1
-mkdir "output\%OUTPUT%" &
+SET OUTPUT=%~2
 call LAStools\bin\e572las.exe -v -i "%INPUT%" -o tmp\out.laz &
-call PotreeConverter.exe tmp\out.laz --overwrite -o "output\%OUTPUT%" &
-call "C:\Program Files\7-Zip\7z.exe" a -t7z "output\%OUTPUT%.zip" "output\%OUTPUT%"
+call PotreeConverter.exe tmp\out.laz --overwrite -o "%OUTPUT%" &
--- autoconverter.ps1
+++ autoconverter.ps1
@@ -1,16 +1,31 @@
 ﻿Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.IO.Compression.FileSystem
-$modelexefolder = 'Z:\BOCH\boch-converter\ATP_720\'
-$cloudpointexefolder = 'Z:\BOCH\boch-converter\ATP_Cloud\'
+Add-Type -AssemblyName System.Windows.Forms 
+
+$Form = New-Object system.Windows.Forms.Form
+$Label = New-Object System.Windows.Forms.ListBox
+$Form.Controls.Add($Label)
+$Form.Text="點雲模型轉檔工具"
+$Label.Items.Add("開始轉檔")
+$Label.Width = $Form.Width
+$Label.Height = $Form.Height
+#$Label.AutoSize = $True
+
+
+$modelexefolder = 'C:\boch-converter\ATP_720\'
+$cloudpointexefolder = 'C:\boch-converter\ATP_Cloud\'
 $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
     InitialDirectory = [Environment]::GetFolderPath('Desktop') 
-    Filter           = '壓縮檔 (*.zip)|*.zip|點雲檔(*.e57)|*.e57'
+    Filter           = '壓縮檔、點雲檔 (*.zip, *.e57)|*.zip;*.e57'
 }
 $dialogresult = $FileBrowser.ShowDialog()
 if ($dialogresult -eq [System.Windows.Forms.DialogResult]::OK) {    
     $tmpfolder = $env:TEMP + '\converter\'
     Remove-Item ($tmpfolder + '*') -Recurse -Force
     if ([System.IO.Path]::GetExtension($FileBrowser.FileName) -eq '.zip') {
+        $Form.Visible = $True        
+        $Label.Items.Add("解壓縮...")
+        $Form.Update()
         [System.IO.Compression.ZipFile]::ExtractToDirectory($FileBrowser.FileName, $tmpfolder)
         $texturepath = Get-ChildItem -Path $tmpfolder -Filter 'texture' -Recurse -ErrorAction SilentlyContinue -Force
         $fbxpath = Get-ChildItem -Path $tmpfolder -Filter '*.fbx' -Recurse -ErrorAction SilentlyContinue -Force
@@ -27,29 +42,44 @@ if ($dialogresult -eq [System.Windows.Forms.DialogResult]::OK) {
         $xmlDoc.LocalSetting.CameraAngle = '1'
         $xmlDoc.Save($xmlpath.FullName)
         Set-Location $modelexefolder
-        Start-Process $exepath.FullName -WindowStyle Minimized -Wait
-        Start-Process $ffmpegpath.FullName -WindowStyle Minimized -Wait
+        $Label.Items.Add("處理模型中...")
+        $Form.Update()
+        Start-Process $exepath.FullName -WindowStyle Hidden -Wait
+        $Label.Items.Add("錄製...")
+        $Form.Update()
+        Start-Process $ffmpegpath.FullName -WindowStyle Hidden -Wait
         Invoke-Item $outputpath.FullName
     } else {
+        $Form.Visible = $True
+        $Form.Update()
         $xmlpath = Get-ChildItem -Path $cloudpointexefolder -Filter 'localSetting.xml' -Recurse -ErrorAction SilentlyContinue -Force
-        $exepath = Get-ChildItem -Path $cloudpointexefolder -Filter 'AutoTakePhoto720.exe' -Recurse -ErrorAction SilentlyContinue -Force
-        $inputpath = Get-ChildItem -Path $cloudpointexefolder -Filter 'Input' -Recurse -ErrorAction SilentlyContinue -Force
-        $outputpath = Get-ChildItem -Path $cloudpointexefolder -Filter 'Output' -Recurse -ErrorAction SilentlyContinue -Force
-        $e572las =  Get-ChildItem -Path $cloudpointexefolder -Filter 'e572las.exe' -Recurse -ErrorAction SilentlyContinue -Force        
-        $PotreeConverter =  Get-ChildItem -Path $cloudpointexefolder -Filter 'PotreeConverter.exe' -Recurse -ErrorAction SilentlyContinue -Force
-
-        Start-Process $e572las -ArgumentList "-v -i $($FileBrowser.FileName) -o $($tmpfolder+'out.laz')" -WindowStyle Minimized -Wait
-        New-Item -ErrorAction Ignore -ItemType directory -Path ($tmpfolder+'output')
-        Start-Process $PotreeConverter -ArgumentList "$($tmpfolder+'out.laz') --overwrite -o $($tmpfolder+'output')" -WindowStyle Minimized -Wait
-
+        $exepath = Get-ChildItem -Path $cloudpointexefolder -Filter 'AutoTakePhoto720.exe' -Recurse -ErrorAction SilentlyContinue -Force 
+        $outputpath = Get-ChildItem -Path $cloudpointexefolder -Filter 'Output' -ErrorAction SilentlyContinue -Force       
+        $inputpath = Get-ChildItem -Path $cloudpointexefolder -Filter 'Input' -ErrorAction SilentlyContinue -Force    
+        $ffmpegpath = Get-ChildItem -Path $cloudpointexefolder -Filter 'ffmpegPNG.bat' -Recurse -ErrorAction SilentlyContinue -Force                     
+        Remove-Item ($inputpath.FullName + '\*') -Recurse -Force
+        Remove-Item ($outputpath.FullName + '\*') -Recurse -Force
+        New-Item -ErrorAction Ignore -ItemType directory -Path ($tmpfolder+'output') | Out-Null          
+        $Label.Items.Add("e57點雲格式轉換中...")
+        $Form.Update()
+        Set-Location 'C:\boch-converter\ATP_Cloud\PointCloudConverter\'
+        Start-Process 'run.bat' -Args """$($FileBrowser.FileName)"" ""$($tmpfolder+'output')""" -WindowStyle Hidden -Wait        
         [xml]$xmlDoc = Get-Content $xmlpath.FullName        
         $xmlDoc.LocalSetting.CloudDataFolder = ($tmpfolder+'output')
         $xmlDoc.LocalSetting.CameraAngle = '1'
         $xmlDoc.Save($xmlpath.FullName)
         Set-Location $cloudpointexefolder
-        Start-Process $exepath.FullName -WindowStyle Minimized -Wait
+        $Label.Items.Add("點雲模型處理中(約需1小時)...")
+        $Form.Update()
+        Start-Process $exepath.FullName -WindowStyle Hidden -Wait  
+        $Label.Items.Add("錄製中...")
+        $Form.Update()
+        Start-Process $ffmpegpath.FullName -WindowStyle Hidden -Wait      
         Invoke-Item $outputpath.FullName
     }        
+    $Label.Text = "完成"
+    Start-Sleep 3
+    $Form.Close()
 }
 else {
     [System.Windows.Forms.MessageBox]::Show('請選擇檔案, 3D模型請用.zip, 點雲模型用.e57.')
